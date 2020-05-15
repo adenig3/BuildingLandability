b8af9d62bd279e45000fd47e2e14cfcbe8817c4c
coolseraj <coolseraj@hotmail.com>
2020-05-14 23:27:57 -0400

    Create UNet.py

---
+++ Models/UNet.py
@@ -0,0 +1,47 @@
+import tensorflow as tf
+from tensorflow import keras
+
+
+class UNet:
+    def __init__(self, num_filters, image_size, regularization_vals):
+        self.num_filters = num_filters
+        self.image_size = image_size
+
+    def configure(self):
+        if len(self.num_filters) != 5:
+            raise Exception("Improper Number of Filters. Alter Model Architecture")
+        inputs = keras.layers.Input((self.image_size, self.image_size, 3))
+        p0 = inputs
+        c1, p1 = self.down_block(p0, self.num_filters[0])
+        c2, p2 = self.down_block(p1, self.num_filters[1])
+        c3, p3 = self.down_block(p2, self.num_filters[2])
+        c4, p4 = self.down_block(p3, self.num_filters[3])
+
+        bn = self.bottleneck(p4, self.num_filters[4])
+
+        u1 = self.up_block(bn, c4, self.num_filters[3])
+        u2 = self.up_block(u1, c3, self.num_filters[2])
+        u3 = self.up_block(u2, c2, self.num_filters[1])
+        u4 = self.up_block(u3, c1, self.num_filters[0])
+
+        outputs = keras.layers.Conv2D(1, (1, 1), padding="same", activation="sigmoid")(u4)
+        model = keras.models.Model(inputs, outputs)
+        return model
+
+    def down_block(self, x, filters, kernel_size=(3, 3), padding="same", strides=1):
+        c = keras.layers.Conv2D(filters, kernel_size, padding=padding, strides=strides, activation="relu")(x)
+        c = keras.layers.Conv2D(filters, kernel_size, padding=padding, strides=strides, activation="relu")(c)
+        p = keras.layers.MaxPool2D((2, 2), (2, 2))(c)
+        return c, p
+
+    def up_block(self, x, skip, filters, kernel_size=(3, 3), padding="same", strides=1):
+        us = keras.layers.UpSampling2D((2, 2))(x)
+        concat = keras.layers.Concatenate()([us, skip])
+        c = keras.layers.Conv2D(filters, kernel_size, padding=padding, strides=strides, activation="relu")(concat)
+        c = keras.layers.Conv2D(filters, kernel_size, padding=padding, strides=strides, activation="relu")(c)
+        return c
+
+    def bottleneck(self, x, filters, kernel_size=(3, 3), padding="same", strides=1):
+        c = keras.layers.Conv2D(filters, kernel_size, padding=padding, strides=strides, activation="relu")(x)
+        c = keras.layers.Conv2D(filters, kernel_size, padding=padding, strides=strides, activation="relu")(c)
+        return c
