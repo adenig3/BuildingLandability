e125c4472c06571bc7171ba116ff78777ea3440d
adenig3 <39922998+adenig3@users.noreply.github.com>
2020-05-28 01:31:06 -0700

    Updated code that works with HyperParamStudy

--- SingleModelRunner.py
+++ SingleModelRunner.py
@@ -19,19 +19,19 @@ inputs_path = 'Inputs/'
 labels_path = 'Labels/'
 #save_path = 'Models/VGG_UNet_Regularized_0.000001.h5'
 #load_path = 'Models/VGG_UNet_Regularized_0.000001.h5'
-load_path = 'Models/UNet_Dropout_0.7.h5'
-save_path = 'Models/UNet_Dropout_0.7.h5'
+load_path = 'Models/UNetn9_0lr0_001dor0_6dr0_0lamb0_0bs32e70.h5'
+save_path = 'Models/UNetn9_0lr0_001dor0_6dr0_0lamb0_0bs32e70.h5'
 #load_path = 'ModelFile.h5'
 
 
 image_size = 192
-epochs = 15
+epochs = 70
 batch_size = 16
 data_pct = [0.95, 0.025, 0.025]  # percent of data for training, validation, and test
 
 # General parameters
 download = False
-sort = True
+sort = False
 make_model = False
 
 
@@ -57,8 +57,8 @@ if make_model:
     y_train = np.expand_dims(y_train2, axis=-1)
     y_val = np.expand_dims(y_val2, axis=-1)
 
-    #model = UNet([16, 32, 64, 128, 256], image_size, 0, 0.8)
-    model = VGG_UNet([16, 32, 64, 128, 256], image_size, 0.000001, 0)
+    model = UNet([16, 32, 64, 128, 256], image_size, 0, 0.71)
+    #model = VGG_UNet([16, 32, 64, 128, 256], image_size, 0.000001, 0)
     for i in range(0,5):
         if i == 0:
             model = model.configure()
@@ -67,6 +67,7 @@ if make_model:
         #model.compile(optimizer=opt, loss="binary_crossentropy", metrics=["acc"])
         #model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["acc"])
         model.compile(optimizer="adam", loss="binary_crossentropy",  metrics=[tf.keras.metrics.MeanIoU(num_classes=2)])
+        iou = tf.keras.metrics.MeanIoU(num_classes=2).result().numpy()
         model.summary()
         model.fit(x=x_train, y=y_train, batch_size=batch_size, epochs=epochs, verbose=2)  # validation_data=[x_val,y_val]
         MU.save_model(model, save_path)
@@ -74,15 +75,19 @@ else:
 
     y_train2 = y_train[:, :, :, 0]
     y_val2 = y_val[:, :, :, 0]
+    x_train, y_train2 = JM.normalize_dataset(x_train,y_train2)
+    x_val, y_val2 = JM.normalize_dataset(x_val,y_val2)
     y_train = np.expand_dims(y_train2, axis=-1)
     y_val = np.expand_dims(y_val2, axis=-1)
     model = MU.load_model(load_path)
     #model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["acc"])
-    model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["acc"])
+    model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["acc",tf.keras.metrics.MeanIoU(num_classes=2)])
+    iou = tf.keras.metrics.MeanIoU(num_classes=2).result().numpy()
     model.summary()
     preds = model.evaluate(x_val, y_val)
     print("Loss = " + str(preds[0]))
     print("Test Accuracy = " + str(preds[1]))
+    print("IoU = ",iou)
 
 MU.show_validation(model,x_val, y_val)
 MU.show_train(model,x_train, y_train)
